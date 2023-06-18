217efecce773e50acc1b997692d1e18d63af3b56
mihaitufescu <mihai.tufescu@gmail.com>
2023-06-18 18:46:11 +0300

    v5

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const path = require("path");
 const port = 8080;
 const app = express();
 const sass = require("sass");
-const {Client} = require("pg");
-const config=({
-    user :'postgres',
-    password : '123456',
-    port : 5432,
-    database : 'postgres'
+const {
+    Client
+} = require("pg");
+var client = new Client({
+    database: "postgres",
+    user: "mihai",
+    password: "123456",
+    host: "localhost",
+    port: 5433
 });
-const pool = new Client(config);
-
-obGlobal={
-    obErori:null,
-    obImagini:null
+client.connect();
+const searchDate = '2023-06-05'; // The date you want to search
+obGlobal = {
+    obErori: null,
+    obImagini: null,
+    cat: null
 }
+
 const folders = ["temp", "temp1", "resurse/backup"];
 const filePaths = {
-    fileSCSS : __dirname + "/resurse/styles/sass",
-    fileCSS : __dirname + "/resurse/styles",
-    filesccs_ejs : __dirname + "/resurse/styles/scss_ejs",
-    fileBackup : __dirname + "/resurse/styles/backup"
+    fileSCSS: __dirname + "/resurse/styles/sass",
+    fileCSS: __dirname + "/resurse/styles",
+    filesccs_ejs: __dirname + "/resurse/styles/scss_ejs",
+    fileBackup: __dirname + "/resurse/styles/backup"
 }
 for (let folder of folders) {
     let folderPath = path.join(__dirname, folder);
@@ -47,6 +52,7 @@ fs.watch(filePaths.fileSCSS, function (event, file) {
             compileCss(filePath);
     }
 })
+
 function compileCss(caleScss, caleCss) {
     if (!caleCss) {
         let fileExtension = path.basename(caleScss);
@@ -63,7 +69,9 @@ function compileCss(caleScss, caleCss) {
     let backupPath = path.join(filePaths.fileBackup, "/resurse/css");
 
     if (!fs.existsSync(backupPath)) {
-        fs.mkdirSync(backupPath, { recursive: true });
+        fs.mkdirSync(backupPath, {
+            recursive: true
+        });
     }
 
     let fileCssCompiled = path.basename(caleScss);
@@ -72,38 +80,134 @@ function compileCss(caleScss, caleCss) {
         fs.copyFileSync(caleCss, path.join(filePaths.fileBackup, fileCssCompiled));
     }
 
-    rez = sass.compile(caleScss, { "sourceMap": true });
+    rez = sass.compile(caleScss, {
+        "sourceMap": true
+    });
     fs.writeFileSync(caleCss, rez.css);
 }
 
 
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 app.set("views", "views/");
 
 app.use(
     express.static(__dirname + "/resurse")
 );
-
-/*app.get("/", (req,res,next)=>{
-    pool.connect(function(err,client,done){
-        if(err){
-            console.log("Failure to connect "+ err);
+app.use(function (req, res, next) {
+    client.query(`SELECT DISTINCT unnest(array_agg(categorie_principala)) AS entry FROM public."Cursuri"`, function (err, rez) {
+        if (err) {
+            console.log(err);
+            afiseazaEroare(res, 2);
         }
-        pool.query("select * from public.test", function(err,rez){
-            console.log("Eroare BD", err);
-            console.log("Rezultat BD", rez);
-        });
-    })
-});*/
-app.get(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function(req,res){
-    initializeazaErori(res,403);
-});
-app.get("/ceva",function(req,res){
-    res.send("<h1>altceva</h1> ip:"+req.ip);
+        var cat = rez.rows;
+        res.locals.cat = cat; // Make `cat` data available to all routes
+        next(); // Proceed to the next middleware or route handler
+    });
 });
 
-app.get(["/","/index", "/home"], function (req, res) {
-    res.render('pagini/index', {ip: req.ip, a:10, b:20, imagini: obGlobal.obImagini.imagini});
+app.get(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function (req, res) {
+    initializeazaErori(res, 403);
+});
+app.get("/cursuri", function (req, res) {
+
+
+    //TO DO query pentru a selecta toate produsele
+    //TO DO se adauaga filtrarea dupa tipul produsului
+    //TO DO se selecteaza si toate valorile din enum-ul categ_prajitura
+    client.query('select * from public."Cursuri"', function (err, rez) {
+        console.log(300);
+        if (err) {
+            console.log(err);
+            afiseazaEroare(res, 2);
+        } else {
+            //console.log(rez);
+            const minQuery = client.query('SELECT MIN(pret) FROM public."Cursuri"');
+            const maxQuery = client.query('SELECT MAX(pret) FROM public."Cursuri"');
+
+            Promise.all([minQuery, maxQuery])
+                .then(([minResult, maxResult]) => {
+                    const minPrice = minResult.rows[0].min;
+                    const maxPrice = maxResult.rows[0].max;
+
+                    res.render("pagini/cursuri", {
+                        cursuri: rez.rows,
+                        optiuni: [],
+                        cat: res.locals.cat,
+                        pret_min: minPrice,
+                        pret_max: maxPrice
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    afiseazaEroare(res,404);
+                });
+        }
+
+    });
+
+
+});
+app.get("/cursuri/:subcategory", function (req, res) {
+    let subcategory = req.params.subcategory;
+    let matchedCategory = null;
+
+    for (let i = 0; i < res.locals.cat.length; i++) {
+        let category = res.locals.cat[i].entry;
+        let normalizedCategory = category.toLowerCase().replace(/\s/g, "");
+
+        if (normalizedCategory === subcategory) {
+            matchedCategory = category;
+            break;
+        }
+    }
+
+    if (matchedCategory) {
+        let query = 'select * from public."Cursuri" where categorie_principala = $1';
+        let values = [matchedCategory];
+
+        client.query(query, values, function (err, result) {
+            if (err) {
+                console.log(err);
+                afiseazaEroare(res, 2);
+            } else {
+                console.log(result);
+                res.render("pagini/cursuri", {
+                    cursuri: result.rows,
+                    optiuni: [],
+                    cat: res.locals.cat
+                });
+            }
+        });
+    } else {
+        console.log("Subcategorie inexistenta");
+        afiseazaEroare(res, 2);
+    }
+});
+
+
+app.get("/curs/:id", function (req, res) {
+    console.log(req.params);
+
+    client.query(`select * from public."Cursuri" where id=${req.params.id}`, function (err, rezultat) {
+        if (err) {
+            console.log(err);
+            afiseazaEroare(res, 2);
+        } else
+            res.render("pagini/curs", {
+                curs: rezultat.rows[0],
+                cat: res.locals.cat
+            });
+    });
+});
+
+app.get(["/", "/index", "/home"], function (req, res) {
+    res.render('pagini/index', {
+        ip: req.ip,
+        a: 10,
+        b: 20,
+        imagini: obGlobal.obImagini.imagini,
+        cat: res.locals.cat
+    });
 });
 
 app.get("/*.ejs", function (req, res) {
@@ -114,60 +218,72 @@ app.get("/favicon.ico", function (req, res) {
     res.sendFile(__dirname + "/res/ico/favicon-32x32.png");
 });
 
-app.get("/*", function(req,res){
-    return res.render('pagini/' + req.url, function(err,result)
-    {
-        if(err)
-        {
-            if(err.message.startsWith("Failed to lookup view"))
+app.get("/*", function (req, res) {
+
+    return res.render('pagini/' + req.url, {
+        cat: res.locals.cat
+    }, function (err, result) {
+        if (err) {
+            if (err.message.startsWith("Failed to lookup view"))
                 afiseazaEroare(res, 404, "ceva");
             else
                 afiseazaEroare(res);
-        }
-        else
-            return  res.send(result);
+        } else
+            return res.send(result);
     });
 });
 
-function initializeazaErori(){
-    var continut=fs.readFileSync(__dirname+"/resurse/json/erori.json").toString('utf8');
+function initializeazaErori() {
+    var continut = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString('utf8');
     obGlobal.obErori = JSON.parse(continut);
-    let vErori=obGlobal.obErori.info_erori;
-    for(let eroare of vErori){
-        eroare.imagine = "/"+obGlobal.obErori.cale_baza+ "/" + eroare.imagine;
+    let vErori = obGlobal.obErori.info_erori;
+    for (let eroare of vErori) {
+        eroare.imagine = "/" + obGlobal.obErori.cale_baza + "/" + eroare.imagine;
     }
 }
 initializeazaErori();
-function afiseazaEroare(res, _identificator, _titlu="titlu default", _text, _imagine){
+
+function afiseazaEroare(res, _identificator, _titlu = "titlu default", _text, _imagine) {
     let vErori = obGlobal.obErori.info_erori;
-    let eroare=vErori.find(function(elem) {return elem.identificator==_identificator;} );
-    if(eroare){
-        let titlu1= _titlu=="titlu default" ? (eroare.titlu || _titlu) : _titlu;
-        let text1= _text || eroare.text;
-        let imagine1= _imagine || eroare.imagine;
-        if(eroare.status)
-            res.status(eroare.identificator).render("pagini/eroare", {titlu:titlu1, text:text1, imagine:imagine1});
+    let eroare = vErori.find(function (elem) {
+        return elem.identificator == _identificator;
+    });
+    if (eroare) {
+        let titlu1 = _titlu == "titlu default" ? (eroare.titlu || _titlu) : _titlu;
+        let text1 = _text || eroare.text;
+        let imagine1 = _imagine || eroare.imagine;
+        if (eroare.status)
+            res.status(eroare.identificator).render("pagini/eroare", {
+                titlu: titlu1,
+                text: text1,
+                imagine: imagine1
+            });
         else
-            res.render("pagini/eroare", {titlu:titlu1, text:text1, imagine:imagine1});
-    }
-    else{
-        let errDef=obGlobal.obErori.eroare_default;
+            res.render("pagini/eroare", {
+                titlu: titlu1,
+                text: text1,
+                imagine: imagine1
+            });
+    } else {
+        let errDef = obGlobal.obErori.eroare_default;
         res.render("pagini/eroare", {
-            titlu:errDef.titlu, text:errDef.text, imagine:obGlobal.obErori.cale_baza+"/"+errDef.imagine
+            titlu: errDef.titlu,
+            text: errDef.text,
+            imagine: obGlobal.obErori.cale_baza + "/" + errDef.imagine
         });
     }
 }
 
-function initializeazaImagini(){
+function initializeazaImagini() {
     var continut = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString('utf8');
     obGlobal.obImagini = JSON.parse(continut);
     const vImagini = obGlobal.obImagini.imagini;
     let caleAbs = path.join(__dirname, "resurse", obGlobal.obImagini.cale_galerie);
     let caleAbsMediu = path.join(caleAbs, "mediu");
-    if(!fs.existsSync(caleAbsMediu)){
+    if (!fs.existsSync(caleAbsMediu)) {
         fs.mkdirSync(caleAbsMediu);
     }
-    for(let imag of vImagini){
+    for (let imag of vImagini) {
         [numeFis, ext] = imag.fisier.split(".");
         imag.fisier_mediu = path.join(obGlobal.obImagini.cale_galerie, "mediu", numeFis + ".webp");
         let caleAbsFisMediu = path.join(__dirname, "resurse", imag.fisier_mediu);
---
+++ resurse/js/cursuri.js
@@ -0,0 +1,140 @@
+window.onload = function () {
+    var myModal = document.getElementById('filtrareModal');
+    var myInput = document.getElementsByClassName('btn-group-vertical');
+    myModal.addEventListener('shown.bs.modal', function () {
+        myInput.focus();
+    });
+    document.getElementById('filtrare').onclick = function () {
+        let denumireCurs = document.getElementById('inp-nume').value.toLowerCase();
+        let allCourses = document.getElementById('i_rad4');
+        let pretRange = parseFloat(document.getElementById('inp-pret').value);
+        let disponibil = document.getElementById('inp-disp').value;
+        let cursuri = document.getElementsByClassName('curs');
+        let categ = document.getElementById('inp-categorie').value;
+        console.log('--------------------------' + cursuri);
+        let rBtns = document.getElementsByClassName('rBtn');
+        let oreDorite;
+        for (let rBtn of rBtns) {
+            if (rBtn.checked) {
+                oreDorite = rBtn.value;
+                break;
+            }
+        }
+
+        let c = 0;
+        for (let curs of cursuri) {
+
+            curs.style.display = 'none';
+
+            let c1;
+            if (denumireCurs != null) {
+
+                let denumire = curs.getElementsByClassName('val-nume')[0].innerHTML;
+                let replacements = {
+                    'ă': 'a',
+                    'â': 'a',
+                    'î': 'i',
+                    'ț': 't',
+                    'ș': 's'
+                };
+
+                for (let rpl in replacements)
+                    denumire = denumire.replace(new RegExp(rpl, 'g'), replacements[rpl]);
+
+                c1 = (denumire.toUpperCase().includes(denumireCurs.toUpperCase()));
+            } else
+                c1 = true;
+
+            let c2;
+            if (curs.getElementsByClassName('val-disponibil')[0].innerHTML === disponibil || disponibil === 'all') {
+                c2 = true;
+            }
+            let c3 = false;
+            let oreCurs = parseInt(curs.getElementsByClassName('val-ore')[0].innerHTML);
+            switch (oreDorite) {
+                case 'mic':
+                    if (oreCurs < 20) {
+                        c3 = true;
+                    }
+                    break;
+                case 'mediu':
+                    if (20 <= oreCurs && oreCurs < 40) {
+                        c3 = true;
+                    }
+                    break;
+                case 'max':
+                    if (oreCurs >= 40) {
+                        c3 = true;
+                    }
+                    break;
+                default:
+                    c3 = true;
+                    break;
+            }
+            let c4;
+            let pretCurs = curs.getElementsByClassName('val-pret')[0].innerHTML;
+            if (pretRange >=  pretCurs) {
+                c4 = true;
+            }
+            let c5;
+            let catCurs = curs.getElementsByClassName('val-categorie')[0].innerHTML;
+            if (categ == catCurs)
+            {
+                c5 = true;
+            }
+             if(categ == 'all'){
+                 c5 = true;
+             }
+            // if (muzica != '---')
+            //     c5 = (produs.getElementsByClassName('list-group-item')[1].getElementsByClassName('badge bg-secondary')[1].innerHTML == muzica);
+            // else
+            //     c5 = true;
+
+            // let c6;
+            // let nrlocuri = parseInt(produs.getElementsByClassName('list-group-item')[4].getElementsByClassName('locuridisp')[0].innerHTML);
+
+            // if (nrlocuri >= locuri)
+            //     c6 = true;
+            // else
+            //     c6 = false;
+
+            // // console.log(c1 + ' ' + c2 + ' ' + c3 + ' ' + c4 + ' ' + c5 + ' ' + c6);
+
+            // if (c1 && c2 && c3 && c4 && c5 && c6) {
+            //     produs.style.display = 'block';
+            //     c ++;
+            // } 
+            if (c1 && c2 && c3 && c4 && c5) {
+                curs.style.display = 'block';
+                c++;
+            }
+        }
+
+        // if (c == 0)
+        //     document.getElementById('noProductsMsg').style.display = 'block';
+        // else
+        //     document.getElementById('noProductsMsg').style.display = 'none';
+    }
+
+    document.getElementById('resetare').onclick = function () {
+        let rBtns = document.getElementsByClassName('rBtn');
+        for (let rBtn of rBtns) {
+            rBtn.checked = false;
+        }
+
+        document.getElementById('inp-nume').value = '';
+        let cursuri = document.getElementsByClassName('curs');
+        for (let curs of cursuri) {
+            curs.style.display = 'block';
+        }
+    };
+    document.getElementById('sortCrescNume').onclick = function (){
+
+    }
+
+    document.getElementById('inp-pret').onchange = function () {
+
+        document.getElementById('valoarePret').value = document.getElementById('inp-pret').value;
+    }
+
+}
